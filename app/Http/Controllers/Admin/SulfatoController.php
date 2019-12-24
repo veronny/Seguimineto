850e3514f501c76b6f0a948cd093a63c12c8254e
veronny <veronny393@hotmail.com>
2019-12-24 13:28:06 -0500

    Graficos de sulfato

--- app/Http/Controllers/Admin/SulfatoController.php
+++ app/Http/Controllers/Admin/SulfatoController.php
@@ -9,6 +9,12 @@ class SulfatoController extends Controller
 {
     Public function index()
     {
-        return view('admin.sulfato.index');   
+        $provincia = array('CHANCHAMAYO', 'CHUPACA', 'CONCEPCION', 'HUANCAYO', 'JAUJA','JUNIN','SATIPO','TARMA','YAULI');
+        $redes = array('','CHANCHAMAYO', 'JAUJA', 'JUNIN', 'NO PERTENECE A NINGUNA RED', 'PICHANAKI','RED DE SALUD CHUPACA','SAN MARTIN DE PANGOA','SATIPO','TARMA','VALLE DEL MANTARO');
+        $data_p  = array(195, 69, 54, 594, 72, 30, 303, 93,22);
+        $data_red = array(9.14,60,80,84,18,69,97,81,77,96,51);
+        return view('admin.sulfato.index',['Provincia' => $provincia, 'Data_p' => $data_p,'Redes' => $redes,'Data_red' => $data_red]);
+                
+        //return view('admin.sulfato.index');   
     }
 }
