9c07df8a0c7189ad081a54e05c7bd99c3884e3da
veronny <veronny393@hotmail.com>
2019-12-26 22:24:55 -0500

    creacion de filtros año y mes

--- app/Http/Controllers/Admin/SulfatoController.php
+++ app/Http/Controllers/Admin/SulfatoController.php
@@ -9,9 +9,13 @@ use App\Sulfato;
 
 class SulfatoController extends Controller
 {
-    Public function index()
+    Public function index(Request $request)
     {
-       // $provincia = 
+        $anno = $request->get('anno');
+        $mes = $request->get('mes');
+        $periodo = $anno.$mes;
+       
+        // $provincia = 
         $provincia = array('CHANCHAMAYO', 'CHUPACA', 'CONCEPCION', 'HUANCAYO', 'JAUJA','JUNIN','SATIPO','TARMA','YAULI');
         $redes = array('','CHANCHAMAYO', 'JAUJA', 'JUNIN', 'NO PERTENECE', 'PICHANAKI','CHUPACA','SM PANGOA','SATIPO','TARMA','V MANTARO');
         $data_p  = array(195, 69, 54, 594, 72, 30, 303, 93,22);
@@ -26,12 +30,13 @@ class SulfatoController extends Controller
                                 SUM(DNI_CUMPLE_HIS) AS DEN,
                                 ROUND((SUM(DNI_CUMPLE_HIS)*100/COUNT(DNI_CUMPLE_HIS)),2) AS PORCENTAJE
                                 FROM indicador_sulfato
+                                WHERE MES = ?
                                 GROUP BY 
                                 PERIODO,
                                 ANNO,
                                 MES,
                                 DISTRITO
-                                ORDER BY PERIODO, PORCENTAJE');
+                                ORDER BY PERIODO, PORCENTAJE',[$mes]);
                
         return view('admin.sulfato.index',['Provincia' => $provincia, 'Data_p' => $data_p,'Redes' => $redes,'Data_red' => $data_red])->with(compact('distrito'));
                 
